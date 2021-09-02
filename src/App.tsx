6ccf54759d60d049cd4544718e0b95185738f5d7
Pedro Takahashi <pedroh.takahashi@gmail.com>
2021-09-02 13:47:23 -0300

    feat9styles): add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Counter from './components/Counter/Counter';
 import RepositoryList from './components/Repository/RepositoryList';
+import  '../src/assets/styles/app.scss';
 
 function App() {
   return (
