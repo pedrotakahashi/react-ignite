142ed05589f0122f1c43d94b4982f31e48b780df
Pedro Takahashi <pedroh.takahashi@gmail.com>
2021-09-01 22:59:04 -0300

    initial projet

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RepositoryList from './components/Repository/RepositoryList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <RepositoryList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
